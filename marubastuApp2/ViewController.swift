90d6a8f12ee935df82256bee3291f512dea929be
naoya0082 <6ds3f7@gmail.com>
2018-10-30 10:57:45 +0900

    Initial Commit

---
+++ marubastuApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  marubastuApp2
+//
+//  Created by 高橋直也 on 2018/10/30.
+//  Copyright © 2018 Takahashi Naoya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
